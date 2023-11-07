12e5b263ea4ee0daac55ef34cb3050ad24bfb9e9
joan <rjoansebastian142@gmail.com>
2023-11-07 16:46:10 -0500

    mi segundo commit

---
+++ suma.js
@@ -0,0 +1,11 @@
+function sumarNumeros() {
+    var num1 = parseFloat(document.getElementById("numero1").value);
+    var num2 = parseFloat(document.getElementById("numero2").value);
+
+    if (!isNaN(num1) && !isNaN(num2)) {
+        var resultado = num1 + num2;
+        document.getElementById("resultado").textContent = resultado;
+    } else {
+        document.getElementById("resultado").textContent = "Ingresa números válidos.";
+    }
+}
